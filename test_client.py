b12414d8b59d3bff15b9ff12700eb5be4a3d3324
sachinkhatter <sachinkhatter@gmail.com>
2020-04-25 10:46:57 -0700

    randmon function

--- test_client.py
+++ test_client.py
@@ -9,3 +9,23 @@
 # Transaction_type
 # Transaction_reason
 # settlement_amount
+import math
+from random import randint
+def _randmon_acct(n):
+    range_start = 10**(n-1)
+    range_end = (10**n) -1
+    return randint(range_start,range_end)
+def _randomize(min,max,type,valid_list):
+    if type in ('acct_num','txn_status','datetime','ref_num','txn_type','txn_reason','amount'):
+        pass
+    else:
+        raise Exception('not a valid type for random function')
+    if type == 'num':
+        return _randmon_acct(max)
+
+if __name__ == '__main__':
+    print(_randmon_acct(16))
+
+
+
+
