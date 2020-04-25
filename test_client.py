00016cd4777f0e314257994d711b359d30a53222
sachinkhatter <sachinkhatter@gmail.com>
2020-04-25 10:39:09 -0700

    random function

---
+++ test_client.py
@@ -0,0 +1,11 @@
+#this is the test client for incoming transactions for settlement
+# Incoming transaction message layout :
+#
+# From_account
+# To_account
+# Transaction_status
+# Transaction_datetime
+# Transaction_ref_num
+# Transaction_type
+# Transaction_reason
+# settlement_amount
