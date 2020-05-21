2d0c4c2736b5341e836fcb3bf2516d61bb99b22e
EC2 Default User <ec2-user@ip-172-31-44-144.us-east-2.compute.internal>
2020-05-21 22:16:45 +0000

    test

--- ledger_microservice/backend/service/test_app/webhook_receiver/controllers/webhook_receiver_controller.py
+++ ledger_microservice/backend/service/test_app/webhook_receiver/controllers/webhook_receiver_controller.py
@@ -4,7 +4,7 @@ from json import dumps, load
 from flask import json, jsonify,request
 import os
 from app.webhook_receiver.services import webhook_reciever_service as wrs
-#from app.preprocessing.services import preprocessing_service as prs
+from app.preprocessing.services import preprocessing_service as prs
 
 blueprint = Blueprint('webhook_receiver_controller',__name__,url_prefix='/receiver')
 webhook_receiver_controller = Api(blueprint)
@@ -28,11 +28,11 @@ class WebhookReceiver(Resource):
 		if dt['status']:
 			response['Web_result'] = dt['data']
 			response['status'] = 'success'
-			# pt=prs.Preprocessing(content)
-			# if pt['status']:
-			# 	response['prs_status'] = 'success'
-			# else:
-			# 	response['prs_status'] = 'failed'
+			pt=prs.Preprocessing(content)
+			if pt['status']:
+				response['prs_status'] = 'success'
+			else:
+				response['prs_status'] = 'failed'
 			temp=json.dumps(response,default=str)
 			response=json.loads(temp)
 			return jsonify(response)
