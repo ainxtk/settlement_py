5d4bae75b205bbceb5fa64f7f782c6db731cb241
sachinkhatter <sachinkhatter@gmail.com>
2020-05-21 13:31:15 -0700

    cleanup config

--- ledger_microservice/backend/service/test_run.py
+++ ledger_microservice/backend/service/test_run.py
@@ -1,11 +1,11 @@
 import unittest
-from test_app import app
+from test_app import test_app
 import json
 
 class BasicTestCase(unittest.TestCase):
 
 	def test_home(self):
-		with app.test_client() as client:
+		with test_app.test_client() as client:
 		    # send data as POST form to endpoint
 		    sent = {'data':'sample record'}
 		    result = client.post('/receiver/post_update',data=json.dumps(sent),content_type='application/json')
