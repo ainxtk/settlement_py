29450bab32549d11f39b03ea2adef7421dfdda7a
EC2 Default User <ec2-user@ip-172-31-44-144.us-east-2.compute.internal>
2020-05-21 22:50:00 +0000

    test

--- ledger_microservice/backend/service/test_app/preprocessing/controllers/webhook_receiver_controller.py
+++ ledger_microservice/backend/service/test_app/preprocessing/controllers/webhook_receiver_controller.py
@@ -3,7 +3,7 @@ from flask_restplus import Api, Resource
 from json import dumps, load
 from flask import json, jsonify,request
 import os
-from app.webhook_receiver.services import webhook_reciever_service as wrs
+from test_app.webhook_receiver.services import webhook_reciever_service as wrs
 
 blueprint = Blueprint('webhook_receiver_controller',__name__,url_prefix='/receiver')
 webhook_receiver_controller = Api(blueprint)
